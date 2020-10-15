f5b2a68eaca6f84587d7bf79163ba91159b85c94
rikyhidayat21 <rikyhidayat21@gmail.com>
2020-10-15 22:42:44 +0700

    editing layout form login

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,36 +1,45 @@
 <template>
   <!-- login-page -->
   <section>
-    <div id="loginregister-container" class="p-3 m-5">
-      <div class="row d-flex mt-5">
-        <div class="col-4 text-light register-login rounded bg-dark">
-          <p class="mt-2 red">Please Login first</p>
-          <form class="my-4" @submit.prevent="login">
-            <div class="form-group ">
-              <label for="email-login">Name</label>
-              <input
-                type="text"
-                class="form-control"
-                name="name"
-                id="name"
-                v-model="name"
-              />
-            </div>
-            <button
-              type="submit"
-              id="btn-login"
-              class="btn btn-light btn-outline-light"
-            >
-              Login
-            </button>
-          </form>
-        </div>
-        <div class="col-8 font-lobster bg-light">
-          <h2 class="text-dark text-center">Welcome to</h2>
-          <h2 class="text-dark text-center">blabla</h2>
+    <div class="container" style="width:70%">
+      <div id="loginregister-container" class="p-5">
+        <div class="row d-flex mt-5 rounded shadow">
+          <div class="col-4 text-light register-login rounded" style="background-color: black;">
+            <img class="col-3 p-0" src="../images/card-gif.gif" alt="" srcset=""><br>
+            <p class="mt-2 red">Please Login first</p>
+            <form class="my-4" @submit.prevent="login">
+              <div class="form-group ">
+                <label for="email-login">Name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="name"
+                  id="name"
+                  v-model="name"
+                />
+              </div>
+              <button
+                type="submit"
+                id="btn-login"
+                class="btn btn-light btn-outline-light"
+              >
+                Login
+              </button>
+            </form>
+          </div>
+          <div class="col-8 font-lobster d-flex align-items-center justify-content-center bg-light rounded shadow" style="opacity: 0.8">
+            <h2 class="text-dark" style="opacity: 1;">
+              <b>
+                Welcome To <br>
+                Matching Card Poker
+              </b>
+            </h2>
+            <!-- <h2 class="text-dark text-center">blabla</h2> -->
+          </div>
         </div>
       </div>
     </div>
+    
   </section>
 </template>
 
