6b5672467d92692820363ba0b807248e6b3a6afb
hutamafs <hutamafajarsaputra@gmail.com>
2020-10-15 18:25:28 +0700

    socket login

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const port = 3000;
 const app = express();
+const port = process.env.PORT || 3000;
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const cors = require('cors');
 
 app.use(cors());
-
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+
+io.on('connection' , (socket) => {
+    console.log('an user connected')
+})
+
+
+http.listen(port,() => {
+    console.log(`running on ${port}`)
+})
+
+
 
